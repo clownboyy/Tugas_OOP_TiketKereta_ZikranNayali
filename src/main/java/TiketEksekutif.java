f387c8a464449f89c0b28555a1b78a05b63939e3
Your Name <you@example.com>
2026-04-09 19:42:52 +0700

    Tugas Pertemuan 3.D

--- src/main/java/TiketEksekutif.java
+++ src/main/java/TiketEksekutif.java
@@ -2,7 +2,7 @@ package com.mycompany.penjualantiketkereta;
 
 public class TiketEksekutif extends Tiket {
     
-    private double biayaLayananTambahan; 
+    private double biayaLayananTambahan;
 
     public TiketEksekutif(String nama, String kursi, double harga, double biayaLayanan) {
         super(nama, kursi, harga);
